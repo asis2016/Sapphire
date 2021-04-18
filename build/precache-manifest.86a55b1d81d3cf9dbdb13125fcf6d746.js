8b2d7efe337533d59326f97c50da5850a2d977d9
Ashish <mhrj.asis@gmail.com>
2021-04-18 05:56:04 +0200

    docs updated

---
+++ build/precache-manifest.86a55b1d81d3cf9dbdb13125fcf6d746.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d01ed5f08260b464094d47ff06be69",
+    "url": "/asis2016/Sapphire-react-bs4/index.html"
+  },
+  {
+    "revision": "7cb1f0e2c68fc77e1e37",
+    "url": "/asis2016/Sapphire-react-bs4/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "04e2d949a590461edd57",
+    "url": "/asis2016/Sapphire-react-bs4/static/css/main.71e6c2ff.chunk.css"
+  },
+  {
+    "revision": "7cb1f0e2c68fc77e1e37",
+    "url": "/asis2016/Sapphire-react-bs4/static/js/2.3e5300c1.chunk.js"
+  },
+  {
+    "revision": "04e2d949a590461edd57",
+    "url": "/asis2016/Sapphire-react-bs4/static/js/main.8d50d5e5.chunk.js"
+  },
+  {
+    "revision": "e0c1440a9ab7818a6562",
+    "url": "/asis2016/Sapphire-react-bs4/static/js/runtime-main.1cf6ea1f.js"
+  },
+  {
+    "revision": "2e7b669bd52e2cfd31fd78f4acbdc4ab",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-1.2e7b669b.jpg"
+  },
+  {
+    "revision": "8e30af9cf867b047dba2140313a6d73a",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-2.8e30af9c.jpg"
+  },
+  {
+    "revision": "2164fa2e06c861b335bff0dbb62717ae",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-3.2164fa2e.jpg"
+  },
+  {
+    "revision": "e51775cba72d10e3c0f5726a566d5edd",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-4.e51775cb.jpg"
+  },
+  {
+    "revision": "ee7b02e07eee7966a2312ab2f34f0fab",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-5.ee7b02e0.jpg"
+  },
+  {
+    "revision": "0a30d8b57aa3c548449e650302adaabf",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/p-6.0a30d8b5.jpg"
+  },
+  {
+    "revision": "97183e25b7ec73320174000f7ff1d14b",
+    "url": "/asis2016/Sapphire-react-bs4/static/media/port-3.97183e25.jpg"
+  }
+]);
