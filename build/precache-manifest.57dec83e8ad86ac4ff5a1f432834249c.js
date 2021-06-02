e007e14eca0201f7e828bdcb137ea6e27ca44f0e
Ashish <mhrj.asis@gmail.com>
2021-06-02 18:48:22 +0200

    update

---
+++ build/precache-manifest.57dec83e8ad86ac4ff5a1f432834249c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3e1aa6051dad2596205d418ea0625a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38fcc48b07fe21524b3f",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "66258693153020cf8877",
+    "url": "/static/css/main.71e6c2ff.chunk.css"
+  },
+  {
+    "revision": "38fcc48b07fe21524b3f",
+    "url": "/static/js/2.929ea0ac.chunk.js"
+  },
+  {
+    "revision": "66258693153020cf8877",
+    "url": "/static/js/main.1da2af23.chunk.js"
+  },
+  {
+    "revision": "a4afaadb585f4548cd92",
+    "url": "/static/js/runtime-main.9cd76b5c.js"
+  },
+  {
+    "revision": "2e7b669bd52e2cfd31fd78f4acbdc4ab",
+    "url": "/static/media/p-1.2e7b669b.jpg"
+  },
+  {
+    "revision": "8e30af9cf867b047dba2140313a6d73a",
+    "url": "/static/media/p-2.8e30af9c.jpg"
+  },
+  {
+    "revision": "2164fa2e06c861b335bff0dbb62717ae",
+    "url": "/static/media/p-3.2164fa2e.jpg"
+  },
+  {
+    "revision": "e51775cba72d10e3c0f5726a566d5edd",
+    "url": "/static/media/p-4.e51775cb.jpg"
+  },
+  {
+    "revision": "ee7b02e07eee7966a2312ab2f34f0fab",
+    "url": "/static/media/p-5.ee7b02e0.jpg"
+  },
+  {
+    "revision": "0a30d8b57aa3c548449e650302adaabf",
+    "url": "/static/media/p-6.0a30d8b5.jpg"
+  },
+  {
+    "revision": "97183e25b7ec73320174000f7ff1d14b",
+    "url": "/static/media/port-3.97183e25.jpg"
+  }
+]);
